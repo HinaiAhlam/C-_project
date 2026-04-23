de3516035a23b09a8fd512d77633ae626ae93a8e
“Ahlam <dreeaam1993@gmail.com>
2026-04-23 14:29:37 +0400

    addsolution12

---
+++ weak1/solution12/project01/Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        RunCalculator(); 
+    }
+
+    static void RunCalculator()
+    {
+        string choice;
+        do
+        {
+            Console.Write("inter first Num ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("inter second Num ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.Write("shose an Op (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            if (op == "+") Console.WriteLine("Result: " + (n1 + n2));
+            else if (op == "-") Console.WriteLine("Result: " + (n1 - n2));
+            else if (op == "*") Console.WriteLine("Result: " + (n1 * n2));
+            else if (op == "/") Console.WriteLine("Result: " + (n1 / n2));
+
+            Console.Write("Another calculation? (yes/no): ");
+            choice = Console.ReadLine().ToLower();
+
+        } while (choice == "yes"); 
+    }
+}
