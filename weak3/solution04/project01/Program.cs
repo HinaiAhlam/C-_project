59bf808d4928884f886a2d7fedff9be3346f71b3
“Ahlam <dreeaam1993@gmail.com>
2026-05-05 15:47:04 +0400

    add file

---
+++ weak3/solution04/project01/Program.cs
@@ -0,0 +1,32 @@
+﻿
+internal class Program
+{
+    class Car
+    {
+        private int temp;
+
+        public int Speed
+        {
+            get { return temp; }
+            set
+            {
+                if (value > 0)
+                {
+                    temp = value;
+                }
+            }
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Driving...");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Car my_car = new Car();
+        my_car.Speed = 400;
+        Console.WriteLine(my_car.Speed);
+    }
+}
